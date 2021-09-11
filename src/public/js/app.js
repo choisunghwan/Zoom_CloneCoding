978a4bec8ed8157c93dc0d4ccd5d99d1ba39fec8
unknown <dute7570@naver.com>
2021-09-11 16:34:29 +0900

    using socket IO

---
+++ src/public/js/app.js
@@ -0,0 +1,16 @@
+const socket = io();
+
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form");
+
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit",handleRoomSubmit);
